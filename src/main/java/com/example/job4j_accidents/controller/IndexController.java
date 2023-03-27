045d8bb421861d874da8a110333d7439e9faed7d
anton-lapin <lapin_antoine@mail.ru>
2023-03-27 21:48:45 +0300

    0. Spring MVC [#6877]

---
+++ src/main/java/com/example/job4j_accidents/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.example.job4j_accidents.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/index")
+    public String index() {
+        return "index";
+    }
+
+}
